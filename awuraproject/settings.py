1afa414de0cd0299a661b1968534ad2cff87faf6
KidusAlemayehu <kidusalemayehu705@gmail.com>
2022-12-13 20:22:23 +0300

    Initial commit

--- awuraproject/settings.py
+++ awuraproject/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 #MEDIA FILES configuration
