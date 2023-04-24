f5423ccae139635e3065df1addfdb4afe404c940
KidusAlemayehu <kidusalemayehu705@gmail.com>
2023-04-24 20:41:18 +0300

    final commit

--- pages/urls.py
+++ pages/urls.py
@@ -4,6 +4,6 @@ urlpatterns = [
     path('',views.landing, name='landing'),
     path('services',views.services, name='services'),
     path('projects',views.projects, name='projects'),
-    path('vacancy', views.vacancy, name='vacancy'),
+    # path('vacancy', views.vacancy, name='vacancy'),
     path('contact',views.contact, name='contact'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -8,7 +8,7 @@ def services(request):
     return render(request,'services.html')
 def projects(request):
     return render(request,'projects.html')
-def vacancy(request):
-    return render(request, 'vacancy.html')
+# def vacancy(request):
+#     return render(request, 'vacancy.html')
 def contact(request):
     return render(request,'contact.html')
